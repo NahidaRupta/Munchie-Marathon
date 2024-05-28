dd733d365776270f5c1aa15901b9f5b1be363990
nahidaRupta <nahida.rahaman37@gmail.com>
2024-05-29 00:38:18 +0600

    routing for all pages complete

---
+++ src/app/pages/menu-pages/menu-pages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-pages',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-pages.component.html',
+  styleUrl: './menu-pages.component.css'
+})
+export class MenuPagesComponent {
+
+}
