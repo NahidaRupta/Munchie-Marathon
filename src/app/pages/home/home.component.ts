e00c352e8c0fc89cdc898ba71101d4e0ff7e1d9e
nahidaRupta <nahida.rahaman37@gmail.com>
2024-05-29 22:57:16 +0600

    nav bar and footer complete

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { OrderDetailsService } from '../../services/order-details.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private service:OrderDetailsService) { }
+
+  foodData:any;
+  ngOnInit(): void {
+    this.foodData = this.service.foodDetails;
+  }
 
 }
